666d20e2247377b5e2d0cdb38948c69e2e5705b6
Ivan Stanevich <ivan_stanevich@waters.com>
2024-02-01 21:29:20 +0100

    add class selection

--- src/badge-highlight/index.mjs
+++ src/badge-highlight/index.mjs
@@ -1,7 +1,8 @@
-import { readFile, open } from "node:fs/promises";
-import { JSDOM } from 'jsdom';
+import {readFile, open} from "node:fs/promises";
+import {JSDOM} from 'jsdom';
 import path from "path";
-import { fileURLToPath } from "url";
+import {fileURLToPath} from "url";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -9,22 +10,33 @@ const FILES_DIR = `${__dirname}/files/`;
 const FILE_PATH = `${__dirname}/files/input.html`;
 const ERROR_MESSAGE = "FS operation failed";
 
-const tags = ['[!NOTE]', '[!TIP]', '[!IMPORTANT]', '[!WARNING]', '[!CAUTION]'];
+const tagClassNameMap = {
+    '[!NOTE]': 'markdown-alert-note',
+    '[!TIP]': 'markdown-alert-tip',
+    '[!IMPORTANT]': 'markdown-alert-important',
+    '[!WARNING]': 'markdown-alert-warning',
+    '[!CAUTION]': 'markdown-alert-caution',
+}
 
+function getTagClassname(tag) {
+    return tagClassNameMap[tag] ?? '';
+}
 
-async function replaceBadges () {
+async function replaceBadges() {
     const content = await readFile(`${FILE_PATH}`, {
         encoding: "utf8",
     });
     const dom = new JSDOM(content);
-    const { document } = dom.window;
+    const {document} = dom.window;
     const blockquotes = [...document.querySelectorAll('blockquote')];
 
-    for(const blockquote of blockquotes) {
-        const { innerHTML } = blockquote;
+    for (const blockquote of blockquotes) {
+        const {innerHTML} = blockquote;
 
-        if (tags.some(t => innerHTML.includes(t))) {
-            blockquote.classList.add('markdown-alert', 'markdown-alert-important');
+        const tag = Object.keys(tagClassNameMap).find(t => innerHTML.includes(t));
+
+        if (tag) {
+            blockquote.classList.add('markdown-alert', getTagClassname(tag));
         }
     }
 
